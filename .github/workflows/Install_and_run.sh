643ffbf2c1b8dc597b97d18031b15d587c6d88ca
Rui He <her2@nih.gov>
2023-01-25 16:53:19 -0500

    Build the Demo

---
+++ .github/workflows/Install_and_run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -l
+
+cd $1
+
+# Check if DESCRIPTION file exist
+
+if [ -f DESCRIPTION ]; then
+    echo "DESCRIPTION exist."
+    
+    Rscript -e 'if(! require("devtools")){install.packages("devtools")};'
+    Rscript -e 'library(devtools);sink(file="test.log");load_all();test();sink()'  
+    
+    echo $(ls)
+    
+    FAIL_num=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 3)
+    
+    if [ $FAIL_num -ne 0 ]; then
+      echo "Number of FAIL in chekc is $FAIL_num"
+      exit 2
+    else
+      echo "Passed Check!"
+    
+
+else 
+    echo "DESCRIPTION file does not exist."
+    exit 1
+fi
+
