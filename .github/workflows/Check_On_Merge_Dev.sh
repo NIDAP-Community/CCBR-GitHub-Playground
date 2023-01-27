c254b23e1f048f266176c7fd4ca5bd797ce54c7f
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-01-27 09:28:28 -0500

    Adding auto-generated NAMESPACE and /man files

---
+++ .github/workflows/Check_On_Merge_Dev.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -l
+
+cd $1
+
+current_dir="$1"
+# Check if DESCRIPTION file exist
+
+if [ -f DESCRIPTION ]; then
+    echo "DESCRIPTION exist."
+    
+    Rscript -e 'if(! require("devtools")){install.packages("devtools")};'
+    Rscript -e 'library(devtools);sink(file="'${current_dir}'/test.log");test();sink()'  
+    
+    cat test.log
+    
+    echo "====================================================================="
+    
+    message_check=$(tail -n 1 test.log | cut -d'|' -f 4 | cut -d' ' -f 2)
+    echo "Message Check: $message_check"
+    
+    if [ "$message_check" = "PASS" ]; then
+      PASS_num=$(tail -n 1 test.log | cut -d'|' -f 4 | cut -d' ' -f 3)
+      if [ "$PASS_num" != "1" ]; then
+        echo "Number of PASS in test is $PASS_num"
+        exit 2
+      else
+        echo "Passed Check!"
+      fi
+      
+    else
+      PASS_num=$(tail -n3 test.log | head -n1 | cut -d'|' -f 4 | cut -d' ' -f 3)
+      
+      if [ "$PASS_num" != "1" ]; then
+        echo "Number of PASS in test is $PASS_num"
+        exit 2
+      else
+        echo "Passed Check!"
+      fi
+    fi
+    
+else 
+    echo "DESCRIPTION file does not exist."
+    exit 1
+fi
+
---
+++ R/Simple_adder.R
@@ -0,0 +1,27 @@
+#' A simple adder code for toy package
+#' @param item_1 first item to be added/combined.
+#' @param item_2 second item to be added/combined.
+#' 
+#' @export Simple_adder
+#' @return This function returns adding for two numeric and join for other scenarios
+#' 
+
+Simple_adder <- function (item_1, item_2){
+  
+  turn_numeric <- function (input){
+    if (suppressWarnings(all(!is.na(as.numeric(as.character(input)))))) {
+      output <- as.numeric(as.character(input))
+    } else {
+      output <- input
+    }
+    return(output)
+  }
+  
+  if (class(turn_numeric(item_1)) != "numeric" || class(turn_numeric(item_2)) != "numeric"){
+    return_item <- paste0(toString(item_1),toString(item_2))
+  }else{
+    return_item <- turn_numeric(item_1) + turn_numeric(item_2)
+  }
+  
+  return(return_item)
+}
