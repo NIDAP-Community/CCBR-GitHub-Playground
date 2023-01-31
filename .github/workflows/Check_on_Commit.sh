b9589c3b662cce882cd213dc5d2879c22c3f1faa
Rui He <her2@nih.gov>
2023-01-30 19:24:09 -0500

    Update method to check what to find in commit

--- .github/workflows/Check_on_Commit.sh
+++ .github/workflows/Check_on_Commit.sh
@@ -3,10 +3,37 @@
 cd $1
 
 current_dir="$1"
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 # Check if DESCRIPTION file exist
+
+#https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-an-array-in-bash
+
+function join_by {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
 
 if [ -f DESCRIPTION ]; then
     echo "DESCRIPTION exist."
+    
+    
+    R_script_test=($(git log -n 1 --raw --name-status --pretty=format: $current_branch | \
+                    grep -E 'tests/testthat' | sed 's:.*/::' ))
+    
+    R_script_func=($(git log -n 1 --raw --name-status --pretty=format: $current_branch | \
+                    grep -E 'R/' | sed 's:.*/::' ))
+    
+    for R_script in ${R_script_func[@]}
+    do
+      test_file=$(ls tests/testthat | grep -E "$R_script")
+      echo $test_file
+      if [[ ! " ${R_script_test[*]} " =~ " ${test_file} " ]]; then
+        R_script_test+="$test_file"
+      fi
+    done
+    
     
     Rscript -e 'if(! require("devtools")){install.packages("devtools")};'
     Rscript -e 'library(devtools);sink(file="'${current_dir}'/test.log");load_all();test();sink()'  
